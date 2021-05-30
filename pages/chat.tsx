3096473392f6ba6d3a785fb012dc1e7f85fde419
Senal Dolage <sdolage@smashydesign.com>
2021-05-30 17:28:35 +0530

    chat Send Message implementation and minor styling updates

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import { Form, Input, Button } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import Notiflix from "notiflix";
 import { db } from '../services/firebase'
+import styles from '../styles/Chat.module.css'
 
 export default function Chat(): JSX.Element {
-    const [chats, setChats] = useState([]);
-    const [content, setContent] = useState();
-    const [readError, setReadError] = useState();
-    const [writeError, setWriteError] = useState();
+    const [chats, setChats] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [form] = Form.useForm();
+    const { TextArea } = Input;
 
     type ChatData = {
         content?: string
@@ -13,29 +17,83 @@ export default function Chat(): JSX.Element {
         uid?: string
     }
 
+    const onFinish = async (values: any) => {
+        try {
+            await db.ref('chats/').push({
+                content: values.message,
+                timestamp: new Date().toLocaleString(),
+                uid: 'JWANJCY4437WRWHQQ135'
+            })
+            Notiflix.Notify.success('Message Sent!');
+            form.resetFields()
+        } catch (error) {
+            Notiflix.Notify.failure('Error sending message');
+            console.error(error)
+        } finally {}
+    };
+
     useEffect(() => {
         const getChatData = async () => {
             try {
-                const chatData = await db.ref('chats/').on('value', snapshot => {
+                await db.ref('chats/').on('value', snapshot => {
                     let chatsArr: ChatData[] = []
                     snapshot.forEach((snap) => {
                         chatsArr.push(snap.val())
                     })
-                    setChats(chatsArr);
+                    setChats(chatsArr)
                 })
             } catch (error) {
-                setReadError(error)
+                console.error(error)
             } finally {
-                console.log("done")
+                setLoading(false);
             }
         }
 
         getChatData();
     }, [])
 
-    console.log(chats)
-
     return (
-        <h1>Chat page</h1>
+        <div className="outter-wrap">
+            <div className="box-container">
+                
+                {loading ?
+                    <LoadingOutlined />
+                    : (
+                        <div className={styles.chat_wrap}>
+                            {chats.map((chatItem) => (
+                                <div key={chatItem.timestamp} className={styles.chat_bubble}>
+                                    <h5 className={styles.chat_message}>{chatItem.content}</h5>
+                                    <p className={styles.chat_timestamp}>{chatItem.timestamp}</p>
+                                </div>
+                            ))}
+                        </div>
+                    )
+                }
+                
+                <Form form={form} name="horizontal_login" onFinish={onFinish} className={styles.message_form}>
+                    <Form.Item
+                        name="message"
+                        rules={[{ required: true, message: 'Please enter a message!' }]}
+                    >
+                        <TextArea showCount maxLength={100} placeholder="Enter message" />
+                    </Form.Item>
+                    <Form.Item shouldUpdate>
+                        {() => (
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                disabled={
+                                    !form.isFieldsTouched(true) ||
+                                    !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                                }
+                            >
+                                Send
+                            </Button>
+                        )}
+                    </Form.Item>
+                </Form>
+
+            </div>
+        </div>
     )
 }
